e74698da06af2d90354848b85fe92ac0f23d1e3f
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2020-12-01 13:32:50 -0300

    adicionada persistência

--- projeto.js
+++ projeto.js
@@ -1,15 +1,16 @@
-const createListItem = (itemText) => {
+const createListItem = ({ checked, text }) => {
   const list = document.getElementById("list");
 
   var item = document.createElement("li");
 
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.checked = checked;
   item.append(checkbox);
 
-  var text = document.createElement("span");
-  text.innerText = itemText;
-  item.append(text);
+  var span = document.createElement("span");
+  span.innerText = text;
+  item.append(span);
 
   var removeButton = document.createElement("input");
   removeButton.type = "button";
@@ -24,10 +25,37 @@ const newItemSubmit = (event) => {
 
   const form = event.target;
   const formData = new FormData(form);
-  createListItem(formData.get("newItemText"));
+  createListItem({ checked: false, text: formData.get("newItemText") });
   form.reset();
 };
 
 document
   .getElementById("newItemForm")
   .addEventListener("submit", newItemSubmit);
+
+const saveList = () => {
+  const list = document.getElementById("list");
+  var serializedList = [];
+
+  for (const item of list.childNodes) {
+    var serializedItem = {};
+    serializedItem.checked = item.querySelector("input[type=checkbox]").checked;
+    serializedItem.text = item.querySelector("span").innerText;
+    serializedList.push(serializedItem);
+  }
+
+  window.localStorage.setItem("list", JSON.stringify(serializedList));
+};
+
+window.addEventListener("beforeunload", saveList);
+
+const loadList = () => {
+  const serializedList = JSON.parse(window.localStorage.getItem("list"));
+  if (serializedList.length > 0) {
+    for (const serializedItem of serializedList) {
+      createListItem(serializedItem);
+    }
+  }
+};
+
+window.onload = loadList;
